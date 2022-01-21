612075f5ae2bd316828089f516f6188609568081
AzizRakhim <azizbekrahimjonov80@gmail.com>
2022-01-21 23:56:22 +0500

    added product lists

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 let elSearchModal = document.getElementById('searchModal');
 let elCloseModal = document.getElementById('closeModal');
 let elModalBox = document.getElementById('modalBox');
+let elUserBtn = document.getElementById('userBtn');
+let elUserModal = document.getElementById('userModal');
+let elUserClose = document.getElementById('userClose');
 
 elSearchModal.addEventListener('click', function() {
     elModalBox.classList.add('show-it');
@@ -8,4 +11,12 @@ elSearchModal.addEventListener('click', function() {
 
 elCloseModal.addEventListener('click', function() {
     elModalBox.classList.remove('show-it');
+})
+
+elUserBtn.addEventListener('click', function() {
+    elUserModal.classList.add('show-it-bitch');
+})
+
+elUserClose.addEventListener('click', function() {
+    elUserModal.classList.remove('show-it-bitch');
 })

13f5ead0e8c9dc8c177c2ee7642dd000c071937f
AzizRakhim <azizbekrahimjonov80@gmail.com>
2022-01-23 20:07:58 +0500

    added fixed btn

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@ let elModalBox = document.getElementById('modalBox');
 let elUserBtn = document.getElementById('userBtn');
 let elUserModal = document.getElementById('userModal');
 let elUserClose = document.getElementById('userClose');
+let elUpwardBtn = document.getElementById('upwardBtn');
+let elUpwardInfoModal = document.getElementById('upwardInfoModal');
+let elMinusBtn = document.getElementById('minusBtn');
 
 elSearchModal.addEventListener('click', function() {
     elModalBox.classList.add('show-it');
@@ -19,4 +22,12 @@ elUserBtn.addEventListener('click', function() {
 
 elUserClose.addEventListener('click', function() {
     elUserModal.classList.remove('show-it-bitch');
+})
+
+elUpwardBtn.addEventListener('click', function() {
+    elUpwardInfoModal.classList.add('dick');
+})
+
+elMinusBtn.addEventListener('click', function() {
+    elUpwardInfoModal.classList.remove('dick');
 })
